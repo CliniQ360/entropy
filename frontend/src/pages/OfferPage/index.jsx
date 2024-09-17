c7a48f873c50a698c5e25816e1ccc8d6064ea70f
Mrunmay Chichkhede <137388316+Mrunmay8558@users.noreply.github.com>
2024-09-17 11:54:50 +0530

    Added Slick

--- frontend/src/pages/OfferPage/index.jsx
+++ frontend/src/pages/OfferPage/index.jsx
@@ -3,16 +3,20 @@ import {
   Divider,
   FormControl,
   FormControlLabel,
-  FormHelperText,
-  FormLabel,
   Grid,
+  IconButton,
   Radio,
   RadioGroup,
+  Stack,
   styled,
-  TextField,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const AvailableOffersContainer = styled(Box)(({ theme }) => ({
   display: "flex",
@@ -70,97 +74,87 @@ const DetailRow = styled("div")(({ theme }) => ({
   marginBottom: theme.spacing(1),
 }));
 
+const ButtonContainer = styled(IconButton)(({ theme, disabled }) => ({
+  height: "30px",
+  width: "30px",
+  justifyContent: "center",
+  border: disabled ? "2px solid #00000042" : "2px solid #000000",
+  alignItems: "center",
+  padding: theme.spacing(2),
+  cursor: disabled ? "not-allowed" : "pointer",
+}));
+
 const AvailableOffersPage = () => {
   const [offers, setOffers] = useState([
     {
       offer_details: {
         offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72e",
-        offer_row_id: 8352,
-        order_id: null,
-        loan_status: "DISCOVERY",
         item_price: "81132.23",
         INTEREST_RATE: "26.99%",
         TERM: "24 Months",
-        INTEREST_RATE_TYPE: "FIXED",
-        APPLICATION_FEE: "0.00 INR",
-        FORECLOSURE_FEE: "4.00% + GST",
-        INTEREST_RATE_CONVERSION_CHARGE: "0",
-        DELAY_PENALTY_FEE: "3.00% + GST",
-        OTHER_PENALTY_FEE: "0",
-        TNC_LINK:
-          "https://www.dmifinance.in/pdf/Loan-Application-Undertaking.pdf",
-        ANNUAL_PERCENTAGE_RATE: "32.23%",
-        REPAYMENT_FREQUENCY: "MONTHLY",
-        NUMBER_OF_INSTALLMENTS_OF_REPAYMENT: "24",
-        COOL_OFF_PERIOD: "P5DT0H0M",
         INSTALLMENT_AMOUNT: "3262.51 INR",
-        CONSENT_HANDLER: "e85a4bbd-9387-4223-b717-f43935b37215",
-        MIN_INTEREST_RATE: "20.0000%",
-        MAX_INTEREST_RATE: "28.0000%",
-        MIN_TENURE: "24 Months",
-        MAX_TENURE: "24 Months",
-        MIN_LOAN_AMOUNT: "5000.0000",
-        MAX_LOAN_AMOUNT: "100000.0000",
       },
       provider_details: {
         name: "DMI FINANCE PRIVATE LIMITED",
         images: [
           {
             url: "https://refo-static-public.s3.ap-south-1.amazonaws.com/dmi/dmi-sm.png",
-            size_type: "sm",
-          },
-          {
-            url: "https://refo-static-public.s3.ap-south-1.amazonaws.com/dmi/dmi-md.png",
-            size_type: "md",
-          },
-          {
-            url: "https://refo-static-public.s3.ap-south-1.amazonaws.com/dmi/dmi-lg.png",
-            size_type: "lg",
           },
         ],
-        short_desc: "DMI FINANCE PRIVATE LIMITED",
-        long_desc: "DMI FINANCE PRIVATE LIMITED",
-        GRO_NAME: "Ashish Sarin",
-        GRO_EMAIL: "head.services@dmifinance.in/grievance@dmifinance.in",
-        GRO_CONTACT_NUMBER: "011-41204444",
-        GRO_DESIGNATION: "Senior Vice President - Customer Success",
-        GRO_ADDRESS:
-          "Express Building, 3rd Floor, 9-10, Bahadur Shah Zafar Marg, New Delhi-110002",
-        CUSTOMER_SUPPORT_LINK: "https://portal.dmifinance.in",
-        CUSTOMER_SUPPORT_CONTACT_NUMBER: "9350657100",
-        CUSTOMER_SUPPORT_EMAIL: "customercare@dmifinance.in",
-        LSP_NAME: "DMI Finance Pvt. Ltd",
-        LSP_EMAIL: "customercare@dmifinance.in",
-        LSP_CONTACT_NUMBER: "9350657100",
-        LSP_ADDRESS:
-          "Express Building, 3rd Floor, 9-10, Bahadur Shah Zafar Marg New Delhi-110002",
       },
-      quote_details: {
-        PRINCIPAL: "60000.00",
-        INTEREST: "18300.23",
-        NET_DISBURSED_AMOUNT: "57168.00",
-        OTHER_UPFRONT_CHARGES: "0",
-        INSURANCE_CHARGES: "0",
-        OTHER_CHARGES: "0",
-        PROCESSING_FEE: "2832.00",
-        quote_price: "81132.23",
-      },
-      payment_details: [],
     },
+    {
+      offer_details: {
+        offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72e",
+        item_price: "81132.23",
+        INTEREST_RATE: "26.99%",
+        TERM: "24 Months",
+        INSTALLMENT_AMOUNT: "3262.51 INR",
+      },
+      provider_details: {
+        name: "DMI FINANCE PRIVATE LIMITED",
+        images: [
+          {
+            url: "https://refo-static-public.s3.ap-south-1.amazonaws.com/dmi/dmi-sm.png",
+          },
+        ],
+      },
+    },
+    // Add more offers as needed
   ]);
-  const [searchText, setSearchText] = useState("");
-  const [formData, setFormData] = useState({
-    aa_id: "",
-    bureauConsent: "",
-  });
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const sliderRef = useRef(null);
 
   const filteredLoanOffers = offers.filter((offer) =>
-    offer.provider_details.name.toLowerCase().includes(searchText.toLowerCase())
+    offer.provider_details.name.toLowerCase().includes("")
   );
 
   const handleInputChange = (event, offer) => {
     event.preventDefault();
     console.log(offer);
+  };
+
+  const next = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const previous = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  const settings = {
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    infinite: false, // Disable infinite scrolling
+    beforeChange: (current, next) => {
+      setCurrentSlide(next);
+    },
+    afterChange: (current) => {
+      setCurrentSlide(current);
+    },
   };
 
   const formatNumber = (num) => {
@@ -186,96 +180,73 @@ const AvailableOffersPage = () => {
       <FormContainer>
         <FormControl component="fieldset">
           <RadioGroup>
-            {filteredLoanOffers ? (
-              filteredLoanOffers.map((offer, index) => (
-                <Box key={index}>
-                  <SelectBoxContainer>
-                    <SelectBoxHeader>
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        {offer.provider_details.images[0].url && (
-                          <img
-                            src={offer.provider_details.images[0].url}
-                            style={{ height: "40px", marginRight: "10px" }}
-                            alt=""
-                          />
-                        )}
-                        <Typography fontSize={18} fontWeight={600}>
-                          {offer.provider_details.name}
-                        </Typography>
-                      </div>
-                      <FormControlLabel
-                        value={offer.offer_details.offer_item_id}
-                        onChange={(e) => handleInputChange(e, offer)}
-                        control={<Radio />}
-                        label=""
-                        style={{ margin: 0 }}
-                      />
-                    </SelectBoxHeader>
-                    <Divider style={{ width: "100%", margin: "10px 0" }} />
-                    <SelectBoxDetails>
-                      <DetailRow>
-                        <span>Loan Amount</span>
-                        <span>
-                          Rs.
-                          {formatNumber(
-                            offer?.quote_details?.SelectedLoanAmount ||
-                              offer?.quote_details?.PRINCIPAL
-                          ) || ""}
-                        </span>
-                      </DetailRow>
-                      <DetailRow>
-                        <span>Interest Rate</span>
-                        <span>
-                          {formatNumber(offer.offer_details.INTEREST_RATE) ||
-                            ""}{" "}
-                          %
-                        </span>
-                      </DetailRow>
-                      <DetailRow>
-                        <span>Tenure</span>
-                        <span>
-                          {formatNumber(offer.offer_details.TERM) || ""} Months
-                        </span>
-                      </DetailRow>
-                      <DetailRow>
-                        <span>Installment Amount</span>
-                        <span>
-                          Rs.
-                          {formatNumber(
-                            offer.offer_details.INSTALLMENT_AMOUNT
-                          ) || ""}
-                          /Month
-                        </span>
-                      </DetailRow>
-                    </SelectBoxDetails>
-                    {/*   <Divider style={{ width: "100%", margin: "10px 0" }} />
-                      <Box
-                        sx={{
-                          display: "flex",
-                          justifyContent: "space-evenly",
-                          width: "100%",
-                        }}
-                      >
-                        <ViewOfferDetails
-                          offer={selectedOffer}
-                          handleDialogButton={() => handleViewDetails(offer)}
-                          open={openDialog.offerDetailsDetails}
-                          setOpen={handleDialogClose}
-                          pageName={"offerPage"}
+            {filteredLoanOffers && filteredLoanOffers.length > 0 ? (
+              <Slider ref={sliderRef} {...settings}>
+                {filteredLoanOffers.map((offer, index) => (
+                  <Box key={index}>
+                    <SelectBoxContainer>
+                      <SelectBoxHeader>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          {offer.provider_details?.images?.[0]?.url && (
+                            <img
+                              src={offer.provider_details.images[0].url}
+                              style={{ height: "40px", marginRight: "10px" }}
+                              alt={
+                                offer.provider_details.name || "Provider Logo"
+                              }
+                            />
+                          )}
+                          <Typography fontSize={18} fontWeight={600}>
+                            {offer.provider_details.name}
+                          </Typography>
+                        </div>
+                        <FormControlLabel
+                          value={offer.offer_details.offer_item_id}
+                          onChange={(e) => handleInputChange(e, offer)}
+                          control={<Radio />}
+                          label=""
+                          style={{ margin: 0 }}
                         />
-                        <Divider orientation="vertical" flexItem />
-                        <CustomizeOfferDetails
-                          offer={selectedOffer}
-                          setNewOfferValues={setNewOfferValues}
-                          handleDialogButton={() => handleCustomizeOffer(offer)}
-                          handleButton={handleSaveChanges}
-                          open={openDialog.customizeOfferDetails}
-                          setOpen={handleDialogClose}
-                        /> 
-                      </Box>*/}
-                  </SelectBoxContainer>
-                </Box>
-              ))
+                      </SelectBoxHeader>
+                      <Divider style={{ width: "100%", margin: "10px 0" }} />
+                      <SelectBoxDetails>
+                        <DetailRow>
+                          <span>Loan Amount</span>
+                          <span>
+                            Rs.{" "}
+                            {formatNumber(offer.offer_details.item_price) || ""}
+                          </span>
+                        </DetailRow>
+                        <DetailRow>
+                          <span>Interest Rate</span>
+                          <span>
+                            {formatNumber(offer.offer_details.INTEREST_RATE) ||
+                              ""}{" "}
+                            %
+                          </span>
+                        </DetailRow>
+                        <DetailRow>
+                          <span>Tenure</span>
+                          <span>
+                            {formatNumber(offer.offer_details.TERM) || ""}{" "}
+                            Months
+                          </span>
+                        </DetailRow>
+                        <DetailRow>
+                          <span>Installment Amount</span>
+                          <span>
+                            Rs.{" "}
+                            {formatNumber(
+                              offer.offer_details.INSTALLMENT_AMOUNT
+                            ) || ""}{" "}
+                            /Month
+                          </span>
+                        </DetailRow>
+                      </SelectBoxDetails>
+                    </SelectBoxContainer>
+                  </Box>
+                ))}
+              </Slider>
             ) : (
               <Box
                 sx={{
@@ -293,6 +264,22 @@ const AvailableOffersPage = () => {
           </RadioGroup>
         </FormControl>
       </FormContainer>
+      <Stack
+        justifyContent={"flex-start"}
+        alignItems={"center"}
+        flexDirection={"row"}
+        gap={2}
+      >
+        <ButtonContainer disabled={currentSlide === 0} onClick={previous}>
+          <KeyboardArrowLeftIcon />
+        </ButtonContainer>
+        <ButtonContainer
+          disabled={currentSlide >= filteredLoanOffers.length - 1}
+          onClick={next}
+        >
+          <ChevronRightIcon />
+        </ButtonContainer>
+      </Stack>
     </AvailableOffersContainer>
   );
 };
