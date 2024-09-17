e02410cdc822be21841f6f64909df34f67c38411
Me-cha <127575583+Me-cha@users.noreply.github.com>
2024-09-17 14:06:07 +0530

    updated offers  page

--- frontend/src/pages/OfferPage/index.jsx
+++ frontend/src/pages/OfferPage/index.jsx
@@ -3,13 +3,13 @@ import {
   Divider,
   FormControl,
   FormControlLabel,
-  Grid,
-  IconButton,
   Radio,
   RadioGroup,
   Stack,
   styled,
   Typography,
+  IconButton,
+  Button,
 } from "@mui/material";
 import React, { useRef, useState } from "react";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
@@ -20,10 +20,8 @@ const AvailableOffersContainer = styled(Box)(({ theme }) => ({
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  //   padding: theme.spacing(4),
-  gap: theme.spacing(6),
   [theme.breakpoints.down("sm")]: {
-    maxWidth: 375,
+    maxWidth: "100%",
   },
 }));
 
@@ -42,15 +40,11 @@ const FormContainer = styled(Box)(({ theme }) => ({
 
 const SelectBoxContainer = styled("div")(({ theme }) => ({
   border: "1px solid #9E9E9E",
-  padding: theme.spacing(2),
-  borderRadius: "5px",
-  boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
-  [theme.breakpoints.down("sm")]: {
-    padding: theme.spacing(1),
-    maxWidth: 300,
-  },
+  padding: theme.spacing(4),
+  borderRadius: "8px",
+  width: 320,
+  backgroundColor: "#FFFFFF",
   flex: "0 0 auto",
-  margin: theme.spacing(2),
 }));
 
 const SelectBoxHeader = styled("div")(({ theme }) => ({
@@ -61,25 +55,26 @@ const SelectBoxHeader = styled("div")(({ theme }) => ({
 }));
 
 const SelectBoxDetails = styled("div")(({ theme }) => ({
-  marginTop: theme.spacing(1),
+  display: "flex",
+  flexDirection: "column",
+  gap: theme.spacing(2),
 }));
 
 const DetailRow = styled("div")(({ theme }) => ({
   display: "flex",
   justifyContent: "space-between",
   width: "100%",
-  marginBottom: theme.spacing(1),
 }));
 
 const ScrollContainer = styled("div")(({ theme }) => ({
   display: "flex",
+  gap: theme.spacing(2),
   overflowX: "auto",
   scrollBehavior: "smooth",
-  padding: theme.spacing(1),
-  maxWidth: "100%", // Ensure it does not exceed viewport width
-  boxSizing: "border-box", // Include padding and border in the element's total width and height
+  maxWidth: "100%",
+  boxSizing: "border-box",
   "&::-webkit-scrollbar": {
-    display: "none", // Hide the scrollbar
+    display: "none",
   },
 }));
 
@@ -114,7 +109,7 @@ const AvailableOffersPage = () => {
     },
     {
       offer_details: {
-        offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72e",
+        offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72f",
         item_price: "81132.23",
         INTEREST_RATE: "26.99%",
         TERM: "24 Months",
@@ -131,7 +126,7 @@ const AvailableOffersPage = () => {
     },
     {
       offer_details: {
-        offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72e",
+        offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72g",
         item_price: "81132.23",
         INTEREST_RATE: "26.99%",
         TERM: "24 Months",
@@ -148,14 +143,14 @@ const AvailableOffersPage = () => {
     },
     {
       offer_details: {
-        offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72e",
+        offer_item_id: "d9eb81e2-96b5-477f-98dc-8518ad60d72h",
         item_price: "81132.23",
         INTEREST_RATE: "26.99%",
         TERM: "24 Months",
         INSTALLMENT_AMOUNT: "3262.51 INR",
       },
       provider_details: {
-        name: "DMI FINANCE PRIVATE LIMITED",
+        name: "Aditya Birla",
         images: [
           {
             url: "https://refo-static-public.s3.ap-south-1.amazonaws.com/dmi/dmi-sm.png",
@@ -166,6 +161,7 @@ const AvailableOffersPage = () => {
   ]);
 
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [selectedOfferId, setSelectedOfferId] = useState(null); // Track selected offer
   const scrollRef = useRef(null);
 
   const filteredLoanOffers = offers.filter((offer) =>
@@ -174,6 +170,7 @@ const AvailableOffersPage = () => {
 
   const handleInputChange = (event, offer) => {
     event.preventDefault();
+    setSelectedOfferId(offer.offer_details.offer_item_id); // Set the selected offer's ID
     console.log(offer);
   };
 
@@ -203,132 +200,216 @@ const AvailableOffersPage = () => {
   };
 
   return (
-    <AvailableOffersContainer>
-      <DocumentHeaderSection>
-        <Typography
-          sx={{ fontSize: "1.4rem", fontWeight: 700, textAlign: "left" }}
-        >
-          Available Offers
-        </Typography>
-        <Typography
-          sx={{ fontSize: "1rem", color: "#535353", textAlign: "left" }}
-        >
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry
-        </Typography>
-      </DocumentHeaderSection>
-      <FormContainer>
-        <FormControl component="fieldset">
-          <RadioGroup>
-            {filteredLoanOffers && filteredLoanOffers.length > 0 ? (
-              <>
-                <ScrollContainer ref={scrollRef}>
-                  {filteredLoanOffers.map((offer, index) => (
-                    <SelectBoxContainer key={index}>
-                      <SelectBoxHeader>
-                        <div style={{ display: "flex", alignItems: "center" }}>
-                          {offer.provider_details?.images?.[0]?.url && (
-                            <img
-                              src={offer.provider_details.images[0].url}
-                              style={{ height: "40px", marginRight: "10px" }}
-                              alt={
-                                offer.provider_details.name || "Provider Logo"
+    <>
+      <AvailableOffersContainer>
+        <DocumentHeaderSection>
+          <Typography
+            sx={{ fontSize: "1.4rem", fontWeight: 700, textAlign: "left" }}
+          >
+            Available Offers
+          </Typography>
+          <Typography
+            sx={{ fontSize: "1rem", color: "#535353", textAlign: "left" }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry
+          </Typography>
+        </DocumentHeaderSection>
+        <FormContainer>
+          <FormControl component="fieldset">
+            <RadioGroup>
+              {filteredLoanOffers && filteredLoanOffers.length > 0 ? (
+                <>
+                  <ScrollContainer ref={scrollRef}>
+                    {filteredLoanOffers.map((offer, index) => (
+                      <SelectBoxContainer key={index}>
+                        <SelectBoxHeader>
+                          <div
+                            style={{ display: "flex", alignItems: "center" }}
+                          >
+                            {offer.provider_details?.images?.[0]?.url && (
+                              <img
+                                src={offer.provider_details.images[0].url}
+                                style={{ height: "24px", marginRight: "10px" }}
+                                alt={"img"}
+                              />
+                            )}
+                            <Typography
+                              fontFamily={"Plus Jakarta Sans Bold"}
+                              fontSize={16}
+                              sx={{ textTransform: "initial" }}
+                            >
+                              {offer.provider_details.name}
+                            </Typography>
+                          </div>
+                          <div>
+                            <FormControlLabel
+                              value={offer.offer_details.offer_item_id}
+                              control={
+                                <Radio
+                                  checked={
+                                    selectedOfferId ===
+                                    offer.offer_details.offer_item_id
+                                  }
+                                />
                               }
+                              onChange={(e) => handleInputChange(e, offer)}
+                              label=""
+                              style={{ margin: 0 }}
                             />
-                          )}
-                          <Typography fontSize={18} fontWeight={600}>
-                            {offer.provider_details.name}
-                          </Typography>
-                        </div>
-                        <FormControlLabel
-                          value={offer.offer_details.offer_item_id}
-                          onChange={(e) => handleInputChange(e, offer)}
-                          control={<Radio />}
-                          label=""
-                          style={{ margin: 0 }}
+                          </div>
+                        </SelectBoxHeader>
+                        <Divider
+                          style={{ width: "100%", margin: "16px 0px" }}
                         />
-                      </SelectBoxHeader>
-                      <Divider style={{ width: "100%", margin: "10px 0" }} />
-                      <SelectBoxDetails>
-                        <DetailRow>
-                          <span>Loan Amount</span>
-                          <span>
-                            Rs.{" "}
-                            {formatNumber(offer.offer_details.item_price) || ""}
-                          </span>
-                        </DetailRow>
-                        <DetailRow>
-                          <span>Interest Rate</span>
-                          <span>
-                            {formatNumber(offer.offer_details.INTEREST_RATE) ||
-                              ""}{" "}
-                            %
-                          </span>
-                        </DetailRow>
-                        <DetailRow>
-                          <span>Tenure</span>
-                          <span>
-                            {formatNumber(offer.offer_details.TERM) || ""}{" "}
-                            Months
-                          </span>
-                        </DetailRow>
-                        <DetailRow>
-                          <span>Installment Amount</span>
-                          <span>
-                            Rs.{" "}
-                            {formatNumber(
-                              offer.offer_details.INSTALLMENT_AMOUNT
-                            ) || ""}{" "}
-                            /Month
-                          </span>
-                        </DetailRow>
-                      </SelectBoxDetails>
-                    </SelectBoxContainer>
-                  ))}
-                </ScrollContainer>
-                <Stack
-                  justifyContent={"center"}
-                  alignItems={"center"}
-                  flexDirection={"row"}
-                  gap={2}
+                        <SelectBoxDetails>
+                          <DetailRow>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              color={"#5A5A5A"}
+                              fontSize={16}
+                            >
+                              Loan Amount
+                            </Typography>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              fontSize={16}
+                            >
+                              Rs.{" "}
+                              {formatNumber(offer.offer_details.item_price) ||
+                                ""}
+                            </Typography>
+                          </DetailRow>
+                          <DetailRow>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              color={"#5A5A5A"}
+                              fontSize={16}
+                            >
+                              Interest Rate
+                            </Typography>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              fontSize={16}
+                            >
+                              {formatNumber(
+                                offer.offer_details.INTEREST_RATE
+                              ) || ""}{" "}
+                              %
+                            </Typography>
+                          </DetailRow>
+                          <DetailRow>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              color={"#5A5A5A"}
+                              fontSize={16}
+                            >
+                              Tenure
+                            </Typography>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              fontSize={16}
+                            >
+                              {formatNumber(offer.offer_details.TERM) || ""}{" "}
+                              Months
+                            </Typography>
+                          </DetailRow>
+                          <DetailRow>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              color={"#5A5A5A"}
+                              fontSize={16}
+                            >
+                              Installment Amount
+                            </Typography>
+                            <Typography
+                              fontFamily={"Source Sans Pro"}
+                              fontSize={16}
+                            >
+                              Rs.{" "}
+                              {formatNumber(
+                                offer.offer_details.INSTALLMENT_AMOUNT
+                              ) || ""}{" "}
+                              /Month
+                            </Typography>
+                          </DetailRow>
+                        </SelectBoxDetails>
+                        <Divider style={{ width: "100%", margin: "16px 0" }} />
+                        <Box
+                          sx={{
+                            display: "flex",
+                            justifyContent: "space-evenly",
+                            width: "100%",
+                          }}
+                        >
+                          <Button
+                            variant="text"
+                            fontFamily={"Source Sans Pro SemiBold"}
+                            fontSize={20}
+                            sx={{ textTransform: "initial" }}
+                          >
+                            {" "}
+                            View Details{" "}
+                          </Button>
+                          <Divider orientation="vertical" flexItem />
+
+                          <Button
+                            variant="text"
+                            fontFamily={"Source Sans Pro SemiBold"}
+                            fontSize={20}
+                            sx={{ textTransform: "initial" }}
+                          >
+                            Customize
+                          </Button>
+                        </Box>
+                      </SelectBoxContainer>
+                    ))}
+                  </ScrollContainer>
+                  <Stack
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    flexDirection={"row"}
+                    gap={2}
+                    m={4}
+                  >
+                    <ButtonContainer
+                      disabled={currentSlide === 0}
+                      onClick={previous}
+                    >
+                      <KeyboardArrowLeftIcon />
+                    </ButtonContainer>
+
+                    <Typography variant="body2" color="textSecondary">
+                      {currentSlide + 1} / {filteredLoanOffers.length}
+                    </Typography>
+
+                    <ButtonContainer
+                      disabled={currentSlide >= filteredLoanOffers.length - 1}
+                      onClick={next}
+                    >
+                      <ChevronRightIcon />
+                    </ButtonContainer>
+                  </Stack>
+                </>
+              ) : (
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
                 >
-                  <ButtonContainer
-                    disabled={currentSlide === 0}
-                    onClick={previous}
-                  >
-                    <KeyboardArrowLeftIcon />
-                  </ButtonContainer>
-
-                  <Typography variant="body2" color="textSecondary">
-                    {currentSlide + 1} / {filteredLoanOffers.length}
+                  <Typography variant="h6" color="textSecondary">
+                    Please try reloading the page by clicking the reload button.
                   </Typography>
-
-                  <ButtonContainer
-                    disabled={currentSlide >= filteredLoanOffers.length - 1}
-                    onClick={next}
-                  >
-                    <ChevronRightIcon />
-                  </ButtonContainer>
-                </Stack>
-              </>
-            ) : (
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography variant="h6" color="textSecondary">
-                  Please try reloading the page by clicking the reload button.
-                </Typography>
-              </Box>
-            )}
-          </RadioGroup>
-        </FormControl>
-      </FormContainer>
-    </AvailableOffersContainer>
+                </Box>
+              )}
+            </RadioGroup>
+          </FormControl>
+        </FormContainer>
+      </AvailableOffersContainer>
+    </>
   );
 };
 
