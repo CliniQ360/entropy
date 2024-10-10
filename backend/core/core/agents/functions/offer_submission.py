7984a163c67a94c984e09b903394e3a3e4a8bd8f
rachitkh4 <130216830+rachitkh4@users.noreply.github.com>
2024-10-10 11:49:06 +0530

    updated logic to fetch max loan amount

--- backend/core/core/agents/functions/offer_submission.py
+++ backend/core/core/agents/functions/offer_submission.py
@@ -573,12 +573,18 @@ def select_offer(state: SahayakState):
             selected_offer = offer
             break
     min_loan_amt = selected_offer.get("offer_details").get("MIN_LOAN_AMOUNT")
-    min_loan_amt = re.sub("[^A-Za-z0-9]+", "", min_loan_amt)
-    min_loan_amt_words = num2words(min_loan_amt)
+    # min_loan_amt = re.sub("[^A-Za-z0-9]+", "", min_loan_amt)
+    # min_loan_amt_words = num2words(min_loan_amt)
     max_loan_amt = selected_offer.get("quote_details").get("PRINCIPAL")
     # max_loan_amt = selected_offer.get("offer_details").get("MAX_LOAN_AMOUNT")
-    max_loan_amt = re.sub("[^A-Za-z0-9]+", "", max_loan_amt)
-    max_loan_amt_words = num2words(max_loan_amt)
+    # max_loan_amt = re.sub("[^A-Za-z0-9]+", "", max_loan_amt)
+    # max_loan_amt_words = num2words(max_loan_amt)
+    min_loan_amt_words = num2words(
+        re.sub(r"[^A-Za-z0-9]+", "", str(min_loan_amt).split(".")[0]), lang="en_IN"
+    )
+    max_loan_amt_words = num2words(
+        re.sub(r"[^A-Za-z0-9]+", "", str(max_loan_amt).split(".")[0]), lang="en_IN"
+    )
     return {
         "agent_message": [
             f"Please select the loan amount you want to avail. You can select any amount between {min_loan_amt_words} rupees and {max_loan_amt_words} rupees."
